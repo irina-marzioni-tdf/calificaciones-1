c7dd7fceb1c8c810ec6f6b4d11d985ffbbf9a521
Kevin <104107277+KevinSantander@users.noreply.github.com>
2022-05-14 17:35:04 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+let 
+//let suma, promedio, contador = Number;
+//contador = 1;
+//suma = 0;
+
+while (contador <= 10) {
+  nota = (Number(dato1.value + (Number(dato2.value + (Number(dato3.value)
+  suma = suma + nota;
+  contador = contador + 1;
+}
+promedio = suma/10;
